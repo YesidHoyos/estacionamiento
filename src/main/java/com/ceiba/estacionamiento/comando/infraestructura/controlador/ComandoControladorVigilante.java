4584a430acaf1106597d2ce4f6b032d661ec3421
Yesid Hoyos <yesid.hoyos@ceiba.com.co>
2019-06-18 14:50:11 -0500

    Se definen los servicios rest con buenas practicas

--- src/main/java/com/ceiba/estacionamiento/comando/infraestructura/controlador/ComandoControladorVigilante.java
+++ src/main/java/com/ceiba/estacionamiento/comando/infraestructura/controlador/ComandoControladorVigilante.java
@@ -4,10 +4,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ceiba.estacionamiento.comando.aplicacion.comando.ComandoTicketVehiculo;
@@ -16,7 +17,7 @@ import com.ceiba.estacionamiento.comando.aplicacion.manejador.ManejadorSacarVehi
 import com.ceiba.estacionamiento.comando.dominio.modelo.TicketVehiculo;
 
 @RestController
-@RequestMapping("/vehiculo")
+@RequestMapping("/parqueadero")
 @CrossOrigin(origins = "*")
 public class ComandoControladorVigilante {
 
@@ -26,13 +27,13 @@ public class ComandoControladorVigilante {
 	@Autowired
 	ManejadorSacarVehiculo manejadorSacarVehiculo;	
 
-	@PostMapping(value = "/ingresar")
+	@PostMapping(value = "/vehiculos")
 	public ComandoTicketVehiculo ingresarVehiculo(@Valid @RequestBody ComandoTicketVehiculo comandoVehiculo) {
 		return manejadorIngresarvehiculo.ingresarVehiculo(comandoVehiculo);
 	}
 	
-	@PostMapping(value = "/sacar")
-	public TicketVehiculo sacarVehiculo(@RequestParam(name = "placa") String placa) {
+	@PutMapping(value = "/vehiculos/{placa}")
+	public TicketVehiculo sacarVehiculo(@PathVariable("placa") String placa) {
 		return manejadorSacarVehiculo.sacarVehiculo(placa);
 	}
 }
