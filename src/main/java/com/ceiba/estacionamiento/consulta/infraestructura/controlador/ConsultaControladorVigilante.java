4f1ca72fa19db26272dfda9e31a9bd7400a91515
Yesid Hoyos <yesidfernandohoyos@gmail.com>
2020-01-09 10:47:25 -0500

    Se cambia el endpoint para obtener los vehiculos ingresados y se expone para ser consumido desde cualquier origen

--- src/main/java/com/ceiba/estacionamiento/consulta/infraestructura/controlador/ConsultaControladorVigilante.java
+++ src/main/java/com/ceiba/estacionamiento/consulta/infraestructura/controlador/ConsultaControladorVigilante.java
@@ -5,13 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ceiba.estacionamiento.consulta.aplicacion.manejador.ManejadorListarVehiculos;
 import com.ceiba.estacionamiento.consulta.dominio.modelo.DtoTicketVehiculo;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/parqueadero")
+@CrossOrigin(origins = "*")
 public class ConsultaControladorVigilante {
 	
 	@Autowired
